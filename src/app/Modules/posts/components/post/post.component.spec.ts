1e111ed9d2c83acc3b789055a2621bffa12c830d
ulbertAO <sp595993@gmail.com>
2023-04-10 08:20:42 +0530

    Test suit using testbed for post component

--- src/app/Modules/posts/components/post/post.component.spec.ts
+++ src/app/Modules/posts/components/post/post.component.spec.ts
@@ -3,6 +3,7 @@
 import { Post } from "src/app/models/Post";
 import { PostComponent } from "./post.component";
 import { first } from "rxjs";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 // import { PostComponent } from './post.component';
 
@@ -27,15 +28,41 @@ import { first } from "rxjs";
 // });
 
 
-describe("Post Component",()=>{
-  it("should raise an event when delete post is clicked",()=>{
-    const component=new PostComponent();
-    const POST:Post={id:1,body:'body 1',title:'title 1'}
+// describe("Post Component",()=>{
+//   it("should raise an event when delete post is clicked",()=>{
+//     const component=new PostComponent();
+//     const POST:Post={id:1,body:'body 1',title:'title 1'}
 
-    component.post=POST;
-    component.delete.pipe(first()).subscribe(selectedPost=>{
-      expect(selectedPost).toBe(POST);
-    })
-    component.onDeletePost(new MouseEvent('click'))
+//     component.post=POST;
+//     component.delete.pipe(first()).subscribe(selectedPost=>{
+//       expect(selectedPost).toBe(POST);
+//     })
+//     component.onDeletePost(new MouseEvent('click'))
+//   });
+// });
+
+
+describe("Post component",()=>{
+  let fixture: ComponentFixture<PostComponent>
+  let component:PostComponent;
+  beforeEach(()=>{
+    TestBed.configureTestingModule({
+      declarations:[PostComponent],
+    });
+    fixture = TestBed.createComponent(PostComponent);
+    component = fixture.componentInstance;
+
+  })
+  it("should create post component using testBed",()=>{
+    expect(component).toBeDefined();
+  });
+    it("should raise an event when delete post is clicked",()=>{
+      const POST:Post={id:1,body:'body 1',title:'title 1'}
+
+      component.post=POST;
+      component.delete.pipe(first()).subscribe(selectedPost=>{
+        expect(selectedPost).toBe(POST);
+      })
+      component.onDeletePost(new MouseEvent('click'))
   });
 });
